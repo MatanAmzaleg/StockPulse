7c0aa2a928b267eeacf9953750eeb52b87e1e1c8
Guy Barda <guy.barda.1234@gmail.com>
2023-02-27 15:08:23 +0200

    added header and sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,14 +3,13 @@ import Link from 'next/link';
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <div className="logo"></div>
-            <nav>
+            <div className="logo">stock pulse</div>
+            <nav className="sidebar-nav">
                 {/* <Link></Link> */}
                 <p>Market</p>
                 <p>Dashboard</p>
                 <p>Setting</p>
                 <p>Crypto Currency</p>
-                <p></p>
             </nav>
         </div>
     );
