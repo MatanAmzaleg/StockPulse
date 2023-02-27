eff3c966a9198b6a595560b7687fae2e82bf814d
Guy Barda <guy.barda.1234@gmail.com>
2023-02-27 16:55:01 +0200

    date update every minute

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
     const [date, setDate] = useState<Date>(new Date());
 
+    useEffect(() => {
+        const intervalId = setInterval(() => setDate(new Date()), 1000 * 60);
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const formmatedDate = () =>
+        date.toLocaleDateString('en-US', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        });
+
     return (
         <div className="main-header">
-            <input type="search" placeholder="Search for stocks market" />
-            <div className="date">Saturday 20 February 2023</div>
+            <input type="text" placeholder="Search for stocks market" />
+            <div className="date">{formmatedDate()}</div>
             <div className="user">
-                <img src="" alt="" />
+                <img src={`https://robohash.org/${11}?set=set5`} alt={'Gi'} />
                 <p className="sidebar-nav-link">GM</p>
             </div>
         </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,35 @@
 import Link from 'next/link';
+import { IoSettingsOutline } from 'react-icons/io5';
+import { AiOutlineBarChart } from 'react-icons/ai';
+import { HiOutlineSquares2X2 } from 'react-icons/hi2';
+import { BsCurrencyBitcoin } from 'react-icons/bs';
 
 export default function Sidebar() {
     return (
-        <div className="sidebar">
-            <div className="logo">stock pulse</div>
-            <nav className="sidebar-nav">
-                {/* <Link></Link> */}
-                <p>Market</p>
-                <p>Dashboard</p>
-                <p>Setting</p>
-                <p>Crypto Currency</p>
-            </nav>
-        </div>
+        <>
+            <div className="logo-container">
+                <div className="logo">stock pulse</div>
+            </div>
+            <div className="sidebar">
+                <nav className="sidebar-nav">
+                    <Link href="/" className="sidebar-nav-link">
+                        <AiOutlineBarChart />
+                        Market
+                    </Link>
+                    <Link href="/" className="sidebar-nav-link">
+                        <HiOutlineSquares2X2 />
+                        Dashboard
+                    </Link>
+                    <Link href="/" className="sidebar-nav-link">
+                        <IoSettingsOutline />
+                        Setting
+                    </Link>
+                    <Link href="/" className="sidebar-nav-link">
+                        <BsCurrencyBitcoin />
+                        Crypto Currency
+                    </Link>
+                </nav>
+            </div>
+        </>
     );
 }
