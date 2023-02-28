f984161a2025e3929a504f37ae587709076b537d
Guy Barda <guy.barda.1234@gmail.com>
2023-02-28 16:39:05 +0200

    refactor

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -7,16 +7,10 @@ import HotCryptoPreview from './HotCryptoPreview';
 export default function MainContent() {
     const { currencies } = useWebSockets(['BTCUSD', 'ETHUSD', 'DOGEUSD']);
 
-    const hotCryptoList = [
-        { title: 'btc', subtitle: 'bitcoin', price: 100, percent: 20 },
-        { title: 'eth', subtitle: 'etherium', price: 100, percent: 20 },
-    ];
-
     return (
         <section className="main-content-sec flex column">
             <h1 className="main-title">Stock Market</h1>
             <p className="subtitle">Trending market group</p>
-            {/* <HotCryptoList hotList={[cur]}></HotCryptoList> */}
             <div className="hot-crypto-list flex">
                 <HotCryptoPreview crypto={currencies.BTCUSD!} />
                 <HotCryptoPreview crypto={currencies.ETHUSD!} />
