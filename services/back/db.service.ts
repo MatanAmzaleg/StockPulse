4ff673e6737c01a694c8f36b9f411c677d3048d3
matan amzaleg <matanamz@gmail.com>
2023-03-01 22:44:05 +0200

    starting to work on users

---
+++ services/back/db.service.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+interface Connection {
+  isConnected?: number;
+}
+
+const connection: Connection = {};
+
+async function connect(): Promise<void> {
+  if (connection.isConnected) {
+    return;
+  }
+
+  const db = await mongoose.connect(`${process.env.MONGO_DB_ATLAS_URL}`);
+
+  connection.isConnected = db.connections[0].readyState;
+}
+
+export default connect;
