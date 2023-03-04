5a0e96ed2ede0cf1327c53b94d1917c1ea1ccc3f
matan amzaleg <matanamz@gmail.com>
2023-03-04 18:23:28 +0200

    working on profile section

--- components/TransactionPreview.tsx
+++ components/TransactionPreview.tsx
@@ -8,17 +8,13 @@ export default function TransactionPreview({ transaction }: Props) {
     console.log(transaction);
     
     return (
-<<<<<<< HEAD
         <div className={transaction.status === 'approved' ? 'transaction-preview approved' : 'transaction-preview'}>
             <p className="action">{transaction.action}</p>
-=======
-        <article className="transaction-preview">
->>>>>>> 68ac61d39e644c8dc9841bacd4dc9ce304274ecf
             <p className="symbol">{transaction.symbol}</p>
             <p className="date">{transaction.date}</p>
             <p className="price">{transaction.price}</p>
             <p className="quantity">{transaction.quantity}</p>
             <p className="status">{transaction.status}</p>
-        </article>
+        </div>
     );
 }
