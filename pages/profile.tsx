a80ffcd13bf4275892afa1b06a6ba25ad7ccd31b
Guy Barda <guy.barda.1234@gmail.com>
2023-03-04 16:06:05 +0200

    added profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,3 +1,29 @@
+import TransactionPreview from '@/components/TransactionPreview';
+
 export default function profile() {
-    return <div>profile</div>;
+    return (
+        <section className="profile-section">
+            <header className="profile-header">
+                <div className="profile-user">
+                    <p className="greet">Good Morning</p>
+                    <h1>Welcom Back, Smith</h1>
+                </div>
+                <div className="user-balance">
+                    <h1>1000$ PulseCoins</h1>
+                    <p>Current balance</p>
+                </div>
+            </header>
+            <div className="profile-grid">
+                <div className="card">hello</div>
+                <div className="card">hello</div>
+                <div className="card">hello</div>
+                <div className="card transaction-container">
+                    <h1>Transacrion History</h1>
+                    <div className="transaction-list">
+                        {/* <TransactionPreview transaction={user.transactions}/> */}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
