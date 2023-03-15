419abfaa86ac11340087354a4ab025f3080cbacb
Guy Barda <guy.barda.1234@gmail.com>
2023-03-15 17:31:08 +0200

    crypto card

--- components/CryptoCard.tsx
+++ components/CryptoCard.tsx
@@ -2,13 +2,13 @@ export default function CryptoCard({ currency, price }: any) {
     const pricePerCoin = () => (price * currency.amount).toFixed(3);
 
     return (
-        <section className="crypto-card-sec">
+        <section className="crypto-card-sec align-center">
             <div className="flex align-center">
                 <img src={`/${currency.symbol}.svg`} alt={currency.symbol} />
                 <p className="bold">{currency.symbolName}</p>
             </div>
-            <p className="scending">{currency.amount}</p>
-            <p className="scending">{pricePerCoin()}</p>
+            <p>{currency.amount}</p>
+            <p>{pricePerCoin()}</p>
             <p className="scending">10% | 88$</p>
         </section>
     );
