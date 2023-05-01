28e94ca494ddce8001aa611fd6713a06c4aaea84
matan amzaleg <matanamz@gmail.com>
2023-05-01 20:37:28 +0300

    working on back finishes

--- components/CryptoCard.tsx
+++ components/CryptoCard.tsx
@@ -1,13 +1,17 @@
 import { calculateChange2, formattedPrice } from "@/utils/format";
+import Link from 'next/link';
 
 export default function CryptoCard({ currency, price, totalBuyAmount }: any) {
   const pricePerCoin = () => (price * currency.amount).toFixed(3);
+console.log(currency);
 
   return (
     <section className="crypto-card-sec align-center">
       <div className="flex align-center">
+      <Link className="div flex align-center" href={`/crypto/${currency.currency}`}>
         <img src={`/${currency.currency}.svg`} alt={currency.currency} />
         <p className="bold">{currency.currency}</p>
+      </Link>
       </div>
       <p>{currency.amount.toFixed("8")}</p>
       <p>{pricePerCoin()}</p>
